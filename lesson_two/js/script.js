464e5e6a0462b3aba91caa8ca9a23cd4b6945be3
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-08-24 20:07:39 +0400

    изменена папка со стилями

--- lesson_two/js/script.js
+++ lesson_two/js/script.js
@@ -11,5 +11,9 @@ console.log(money, income, deposit);
 console.log(income.length);
 console.log("Период " + period + " месяцев");
 console.log(addExpenses.toLowerCase().split(' '));
-
 console.log(budgetDay);
+
+let num = 266219;
+
+
+console.log((num[0] * num[1] * num[2] * num[3] * num[4] * num[5]));
