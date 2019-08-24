453a2b12e238faf0a83b789b08603688b4c89a33
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-08-24 21:09:38 +0400

    исправлено задание по делению и остатку

--- lesson_two/js/script.js
+++ lesson_two/js/script.js
@@ -5,14 +5,15 @@ let deposit = true;
 let mission = 65250;
 let period = 7;
 
-let budgetDay = money%30;
+let budgetDay = money/30;
+let	budgetDay2 = money%30;
 
 console.log(money, income, deposit);
 console.log(income.length);
 console.log("Период " + period + " месяцев");
 console.log(addExpenses.toLowerCase().split(' '));
-console.log(budgetDay);
-
+console.log("результат: "+	budgetDay);
+console.log("остаток от деления: " + budgetDay2);
 
 
 
