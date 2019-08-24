92e7b4bc9fe29cf5c3a0ed7d8b68fb4e5e151dd4
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-08-24 20:39:26 +0400

    изменен задание на остаток от деления

--- lesson_two/js/script.js
+++ lesson_two/js/script.js
@@ -5,7 +5,7 @@ let deposit = true;
 let mission = 65250;
 let period = 7;
 
-let budgetDay = money/30;
+let budgetDay = money%30;
 
 console.log(money, income, deposit);
 console.log(income.length);
@@ -13,7 +13,6 @@ console.log("Период " + period + " месяцев");
 console.log(addExpenses.toLowerCase().split(' '));
 console.log(budgetDay);
 
-let num = 266219;
 
 
-console.log((num[0] * num[1] * num[2] * num[3] * num[4] * num[5]));
+
