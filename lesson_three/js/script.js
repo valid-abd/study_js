1ce0d88611b311a537b3ce8eedf329000153d76a
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-08-28 01:14:21 +0400

    исправлены ошибки в коде

--- lesson_three/js/script.js
+++ lesson_three/js/script.js
@@ -1,13 +1,15 @@
+"use strict";
+/*jshint esversion: 6 */
 let money = prompt('Ваш месячный доход?');
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 let deposit = confirm('Есть ли у вас депозит в банке?');
 
-let questions = prompt("Какие обязательные ежемесячные расходы у вас есть?", );
-		questions2 = prompt('Во сколько это обойдется?');
-		questions = prompt("Какие обязательные ежемесячные расходы у вас есть?", );
-		questions2 = prompt('Во сколько это обойдется?');
-let mission = 65250;  // моя цель 
-let budgetMonth = money - questions;  // чистая прибыль
+let questions = prompt("Какие обязательные ежемесячные расходы у вас есть?");
+let questions2 = prompt('Во сколько это обойдется?');
+let questions = prompt("Какие обязательные ежемесячные расходы у вас есть?");
+let questions2 = prompt('Во сколько это обойдется?');
+let mission = 65250; // моя цель 
+let budgetMonth = money - questions2; // чистая прибыль
 
 let budgetDay = budgetMonth / 30; // бюджет на месяц 
 
@@ -18,16 +20,6 @@ console.log('Доход за месяц с вычетам расхода : ' + b
 console.log('Моя цель будет достигаться месяцев: ' + Math.ceil(mission / budgetMonth));
 console.log('Мой дневной бюджет : ' + Math.floor(budgetDay));
 
-// if (budgetDay >=800) {
-// 	console.log('Высокий уровень дохода');
-// } else if (budgetDay >= 300 ){
-// 	console.log('Средный зароботок');
-// } else if (budgetDay >= 0) {
-// 	console.log(' Бедняг ');
-// }
-// 	else {
-// 	console.log('Что то пошло не так');
-// }
 
 let cmd = (budgetDay >= 800) ? ' Высокий уровень дохода ' :
 	(budgetDay >= 300) ? ' Средний уровень дохода ' :
@@ -35,4 +27,5 @@ let cmd = (budgetDay >= 800) ? ' Высокий уровень дохода ' :
 
 	' Что то пошло не так ';
 
-	console.log(cmd );
+console.log(cmd);
+
