7982fd4073c646730002736a9fcbaa94242d57e9
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-09-04 20:56:00 +0400

    исправлены ошибки в переменной addExpenses

--- Practice/js/script.js
+++ Practice/js/script.js
@@ -40,8 +40,9 @@
 				appData.income[itemIncome] = cashIncome;
 				appData.income[itemIncome] = itemIncome;
 			}
-			let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'аренда питание');
-			appData.addExpenses = addExpenses.toLowerCase().split(',');
+			let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'аренда, питание');
+			appData.addExpenses = addExpenses.toLowerCase().split(',').map(function (item) {
+				return item.trim()});
 			appData.deposit = confirm('Есть ли у вас депозит в банке?');
 			for (let i = 0; i < 2; i++) {
 				let questions1;
@@ -119,8 +120,7 @@
 		console.log(' Наша программа включает в себя данные: ' + key + ' - ' + appData[key]);
 	}
 
-// appData.addExpenses = addExpenses.split(',').map(function (item) {
-// 			return item.trim()
+
 	console.log(appData.addExpenses.map(function (item) {
 			return item[0].toUpperCase() + item.slice(1);
 		 }).join(', '));
