819c7e74ce8257b52b8c5b0f799ae56823bb2476
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-09-06 04:34:48 +0400

    изменено в переменной махбаттон

---
+++ lesson_07/budget_app__copy/js/script.js
@@ -0,0 +1,42 @@
+
+"use strict";
+/*jshint esversion: 6 */
+
+let button = document.querySelector('#start');
+console.log(button);
+
+let MaxButton = document.getElementsByTagName('button')[0];
+let MaxButton2 = document.querySelectorAll('button')[1];
+console.log('MaxButton: ', MaxButton, MaxButton2);
+
+let checkbox = document.querySelectorAll('#deposit-check');
+console.log('checkbox: ', checkbox);
+
+let entryField = document.querySelectorAll('.additional_income-item');
+console.log('entryField: ', entryField);
+
+let results = document.querySelectorAll('.result-total');
+console.log('results: ', results);
+
+let Myinpu01 = document.querySelector('.salary-amount');
+console.log('Myinpu01: ', Myinpu01);
+let Myinpu02 = document.querySelector('input[class=income-title]');
+console.log('Myinpu02: ', Myinpu02);
+let Myinpu03 = document.querySelector('.income-amount');
+console.log('Myinpu03: ', Myinpu03);
+let Myinpu04 = document.querySelector('input[class=expenses-title]');
+console.log('Myinpu04: ', Myinpu04);
+let Myinpu05 = document.querySelector('.expenses-amount');
+console.log('Myinpu05: ', Myinpu05);
+let Myinpu06 = document.querySelector('.deposit-amount');
+console.log('Myinpu06: ', Myinpu06);
+let Myinpu07 = document.querySelector('.deposit-percent');
+console.log('Myinpu07: ', Myinpu07);
+let Myinpu08 = document.querySelector('.target-amount');
+console.log('Myinpu08: ', Myinpu08);
+let Myinpu09 = document.querySelector('.period-select');
+console.log('Myinpu09: ', Myinpu09);
+let Myinpu10 = document.querySelector('.additional_expenses-item');
+console.log('Myinpu10: ', Myinpu10);
+
+
