847146f8356932799212207383abcfe0b5a7e6e0
valid-abd <44767085+valid-abd@users.noreply.github.com>
2019-09-17 21:06:39 +0400

    удален куб с 10 урока

--- lesson_10/js/script.js
+++ lesson_10/js/script.js
@@ -29,50 +29,6 @@ let PeriodSelect = document.querySelector('.period-select');
 let AdditionalExpensesItem = document.querySelector('.additional_expenses-item');
 let incomeItems = document.querySelectorAll('.income-items');
 
-const DomElement = function (options, text) {
-	this.selector = options.selector;
-	this.height = options.height;
-	this.width = options.width;
-	this.bg = options.bg;
-	this.fontSize = options.fontSize;
-	this.text = text;
-};
-
-DomElement.prototype.createElement = function(){
-	if(this.selector[0] === '.'){
-		const div = document.createElement('div');
-		div.style.cssText =`
-			height: ${this.height};
-			width: ${this.width};
-			background: ${this.bg};
-			fonst-size: ${this.fontSize};
-		`;
-		div.textContent=this.text; 
-		document.body.appendChild(div);
-	} else if (this.selector[0] === '#') {
-		const _p = document.createElement('p');
-		_p.style.cssText = `
-			height: ${this.height};
-			width: ${this.width};
-			background: ${this.bg};
-			fonst-size: ${this.fontSize};
-		`;
-		_p.textContent = this.text;
-		document.body.appendChild(p);
-	};
-};
-
-const optionSquareRed = {
-	selector: '.square-red',
-	height: '150px',
-	width: '150px',
-	bg: 'red',
-	fontSize: '15px'
-};
-
-const square = new DomElement(optionSquareRed, 'Hello');
-square.createElement();
-
 
 const AppData = function(){
 
